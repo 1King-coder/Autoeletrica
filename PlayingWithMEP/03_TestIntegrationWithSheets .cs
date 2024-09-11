776e313652e494ed39a6e5f5b9fbfeb0c38d7efa
Vitor dos Santos Barcelos <progvitor@gmail.com>
2024-09-11 16:51:50 -0300

    Implemented google sheets api authentication and write data method

---
+++ PlayingWithMEP/03_TestIntegrationWithSheets .cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Electrical;
+using ECs = PlayingWithMEP.ElectricalClasses;
+
+
+namespace PlayingWithMEP
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class _03_TestIntegrationWithSheets : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elementSet)
+        {
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+            Selection sel = uiapp.ActiveUIDocument.Selection;
+
+            GoogleSheetsManager sheetsApi = new GoogleSheetsManager("1MZt_KFsS692brVrzg6c-06q4siw1l7bPfPLvwIXfC_c");
+            
+
+            Utils utils = new Utils(doc);
+
+            Transaction trans = new Transaction(doc);
+
+            trans.Start("Selection");
+
+            FamilyInstance el = utils.pickElement(sel);
+            
+            trans.Commit();
+
+            ECs.Panel panel = new ECs.Panel(el, doc);
+
+            List<object> data = new List<object>() { "Hello", "World", "1001" };
+
+            sheetsApi.writeData("Quadro de Carga", "B8:D8", data);            
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ PlayingWithMEP/GoogleSheetsManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4.Data;
+using System.IO;
+
+
+namespace PlayingWithMEP
+{
+    internal class GoogleSheetsManager
+    {
+        private readonly string[] scopes = { SheetsService.Scope.Spreadsheets };
+
+        private readonly string AppName = "Automacao Planilha de Dimensionamento eletrico";
+
+        private string SpreadsheetId;
+
+        public SheetsService service { get; set; }
+
+        public GoogleSheetsManager(string spreadsheetId)
+        {
+            this.SpreadsheetId = spreadsheetId;
+
+            this.service = GetSheetsService();
+
+        }
+
+        public SheetsService GetSheetsService()
+        {
+            GoogleCredential credential;
+
+            
+            
+            using (var stream = new FileStream("C:\\Users\\vibar\\Source\\Repos\\1King-coder\\PlayingWithRevitMEP\\PlayingWithMEP\\credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(scopes);
+            }
+
+            SheetsService service = new SheetsService(new BaseClientService.Initializer() {
+                HttpClientInitializer = credential,
+                ApplicationName = this.AppName
+            });
+            
+            return service;
+        }
+
+        public void writeData(string sheet, string range, List<object> data)
+        {
+            ValueRange valuesToSend = new ValueRange();
+
+            List<IList<object>> dataMatrix = new List<IList<object>> { data };
+
+            valuesToSend.Values = dataMatrix;
+
+            var appendReq = this.service.Spreadsheets.Values.Append(valuesToSend, this.SpreadsheetId, $"{sheet}!{range}");
+            appendReq.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+            
+            appendReq.Execute();
+
+        
+        }
+
+
+    }
+}
